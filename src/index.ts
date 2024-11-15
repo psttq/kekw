b69033cc8f79a54b2646710b3ca2ce0c696deff9
pstt <xyamix1337@gmail.com>
2024-11-15 23:08:21 +0300

    shoot

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import * as PIXI from "pixi.js";
 
+document.body.style.backgroundColor = "#181818";
+
+function removeItem<T>(arr: Array<T>, value: T): Array<T> {
+  const index = arr.indexOf(value);
+  if (index > -1) {
+    arr.splice(index, 1);
+  }
+  return arr;
+}
+
 const main = async () => {
   // Main app
   let app = new PIXI.Application();
@@ -8,9 +18,13 @@ const main = async () => {
   document.body.appendChild(app.canvas);
 
   // Create the sprite and add it to the stage
-  await PIXI.Assets.load("../assets/hello-world.png");
-  let sprite = PIXI.Sprite.from("../assets/hello-world.png");
+  await PIXI.Assets.load("../assets/player_0.png");
+  let sprite = PIXI.Sprite.from("../assets/player_0.png");
   sprite.setSize(100, 100);
+
+  sprite.texture.source.magFilter = "nearest";
+  sprite.texture.source.minFilter = "nearest";
+
   app.stage.addChild(sprite);
 
   let vx = 0;
@@ -50,15 +64,46 @@ const main = async () => {
 
   sprite.x = 300;
   sprite.y = 300;
-  let pivot = new PIXI.Circle(
-    sprite.x + sprite.pivot._x,
-    sprite.y + sprite.pivot._y,
-    4
-  );
 
-  app.stage.addChild(pivot);
+  sprite.anchor.set(0.5);
 
-  sprite.pivot.set(200, 200);
+  let mouse_x = 0;
+  let mouse_y = 0;
+
+  sprite.on("globalmousemove", (event: PIXI.FederatedPointerEvent) => {
+    mouse_x = event.x;
+    mouse_y = event.y;
+  });
+  sprite.eventMode = "dynamic";
+
+  let bullets: {
+    v_x: number;
+    v_y: number;
+    life: number;
+    geom: PIXI.Graphics;
+  }[] = [];
+
+  let bullet_speed = 10.0;
+
+  document.addEventListener("mousedown", (e: MouseEvent) => {
+    let vec_x = mouse_x - sprite.x;
+    let vec_y = mouse_y - sprite.y;
+
+    let vec_length = Math.sqrt(vec_x * vec_x + vec_y * vec_y);
+    vec_x /= vec_length;
+    vec_y /= vec_length;
+
+    let grpx = new PIXI.Graphics().circle(sprite.x, sprite.y, 2).fill("red");
+    app.stage.addChild(grpx);
+
+    bullets.push({
+      v_x: vec_x * bullet_speed,
+      v_y: vec_y * bullet_speed,
+      life: 50,
+      geom: grpx
+    });
+  });
+
   // Add a ticker callback to move the sprite back and forth
   let elapsed = 0.0;
   app.ticker.add((ticker) => {
@@ -66,7 +111,28 @@ const main = async () => {
     sprite.x += vx * ticker.deltaTime;
     sprite.y += vy * ticker.deltaTime;
 
-    sprite.rotation += 0.01 * ticker.deltaTime;
+    let vec_x = mouse_x - sprite.x;
+    let vec_y = mouse_y - sprite.y;
+
+    let vec_length = Math.sqrt(vec_x * vec_x + vec_y * vec_y);
+    vec_x /= vec_length;
+    vec_y /= vec_length;
+
+    let angle = Math.acos(vec_x);
+    if (vec_y < 0) angle *= -1;
+
+    for (let bullet of bullets) {
+      if (bullet.life < 0) {
+        app.stage.removeChild(bullet.geom);
+        removeItem(bullets, bullet);
+      }
+      bullet.geom.x += bullet.v_x * ticker.deltaTime;
+      bullet.geom.y += bullet.v_y * ticker.deltaTime;
+
+      bullet.life -= ticker.deltaTime;
+    }
+
+    sprite.rotation = angle;
   });
 };
 
